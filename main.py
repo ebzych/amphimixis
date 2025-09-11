af050bbdd77ceafbae91fa15f57a8421564c2988
Dmitrii Bordukov <dabordukov@mail.ru>
2025-09-11 17:59:39 +0300

    chore: deleted useless file

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from analyzer import Analyzer
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <repo_path>")
-        sys.exit(1)
-    analyzer = Analyzer(sys.argv[1])
-    analyzer.analyze()
