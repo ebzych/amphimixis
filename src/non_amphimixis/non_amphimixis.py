7fe8c4d4bdcfcee54b4acd22e92e3e1f0b8d0f6b
Dmitrii Bordukov <dabordukov@mail.ru>
2025-09-12 19:57:04 +0300

    fix(Project): correct default builds path in constructor

--- src/non_amphimixis/non_amphimixis.py
+++ src/non_amphimixis/non_amphimixis.py
@@ -1,6 +1,9 @@
+import os
+
+
 class Project:
-    def __init__(self, repo_path: str, builds_path: str = "builds/"):
+    def __init__(self, repo_path: str, builds_path: str = "build/"):
         self.path = repo_path
-        self.builds_path = builds_path
+        self.builds_path = os.path.join(repo_path, builds_path)
         self.build_system = "cmake"
         self.targets = []
