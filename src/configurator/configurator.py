c6d961e444657e4781bb244518522a6ec50142af
maria <novikmaria36@gmail.com>
2025-09-13 01:19:27 +0300

    refactor: rename the module 'nonamphimixis' to 'general'

--- src/configurator/configurator.py
+++ src/configurator/configurator.py
@@ -1,8 +1,8 @@
-from non_amphimixis import non_amphimixis
+from NonAmphimixis.src.general import general
 import json
 
 
-def configure(project: non_amphimixis.Project):
+def configure(project: general.Project):
     args = []
     while True:
         arg = input("Enter arg\n")
