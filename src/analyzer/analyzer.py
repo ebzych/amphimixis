fbac0f07c88d60e2931deefc40da654e31c3a415
Julia <juliaadod@gmail.com>
2025-09-15 03:45:22 +0900

    docs: updated docs for analyzer feat: added function create_project_file

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -1,6 +1,16 @@
+"""Module that analyzes project's repository and creates file with its information"""
+
+
 class Analyzer:
+    """Class that analyzes project's repository and creates file with its information"""
+
     def __init__(self, repo_path: str):
         self.repo_path = repo_path
 
     def analyze(self):
+        """Analyzes project and collects its information"""
         print(f"Analyzing {self.repo_path}")
+
+    def create_project_file(self):
+        """Creates file with information about project"""
+        raise NotImplementedError
