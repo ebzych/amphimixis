970dcfeb10f8b02dbac9df25929e75187b9e05f2
Dmitrii Bordukov <dabordukov@mail.ru>
2025-09-14 21:17:05 +0300

    fix: general

--- src/general/general.py
+++ src/general/general.py
@@ -1,14 +1,4 @@
 import os
-<<<<<<< HEAD
-
-
-class Project:
-    def __init__(self, repo_path: str, builds_path: str = "build/"):
-        self.path = repo_path
-        self.builds_path = os.path.join(repo_path, builds_path)
-        self.build_system = "cmake"
-        self.targets = []
-=======
 from enum import Enum
 
 
@@ -19,6 +9,7 @@ class Arch(Enum):
     RISCV = 1
     ARM = 2
 
+
 class BuildSystem(Enum):
     """Supported build systems"""
 
@@ -26,21 +17,25 @@ class BuildSystem(Enum):
     CMAKE = 1
     NINJA = 2
 
+
 class Build:
     """Class with information about one build of project"""
 
-    def __init__(self, build_path       : str,
-                    # path to directory with this build;
-                    # directory { dir:builded_project, file:config.json, file:build.log, ... }
-                 arch                   : Arch,
-                 is_specified_script    : bool,
-                    # True if user specify build script
-                    #               and False if script is simple: configuration -> build
-                 build_system           : BuildSystem,              # is high-level build system
-                 config_flags           : str,
-                 cxx_flags              : str,
-                 c_flags                : str,
-                 runner                 : BuildSystem):             # is low-level build system
+    def __init__(
+        self,
+        build_path: str,
+        # path to directory with this build;
+        # directory { dir:builded_project, file:config.json, file:build.log, ... }
+        arch: Arch,
+        is_specified_script: bool,
+        # True if user specify build script
+        #               and False if script is simple: configuration -> build
+        build_system: BuildSystem,  # is high-level build system
+        config_flags: str,
+        cxx_flags: str,
+        c_flags: str,
+        runner: BuildSystem,
+    ):  # is low-level build system
         self.build_path = build_path
         self.arch = arch
         self.is_specified_script = is_specified_script
@@ -50,11 +45,10 @@ class Build:
         self.c_flags = c_flags
         self.runner = runner
 
+
 class Project:
     """Class with information about project and his builds"""
 
-    def __init__(self, repo_path: str,
-                 builds : list[Build]):
+    def __init__(self, repo_path: str, builds: list[Build]):
         self.path = repo_path
-        self.builds = []
->>>>>>> c5d5dcf (refactor: add Build class, Arch and BuildSystem enums, edit Project class fields: list[Build] instead str /path/to/builds)
+        self.builds = builds
