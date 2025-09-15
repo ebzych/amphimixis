ba446942f299b806f9afa86731a8a58c1cde0aa9
ebzych <bzychkin.egor@gmail.com>
2025-09-15 21:10:35 +0300

    docs: add conditions of the return value by IBuildSystem functions

--- src/general/general.py
+++ src/general/general.py
@@ -23,15 +23,19 @@ class IBuildSystem(ABC):
 
     @staticmethod
     @abstractmethod
-    def insert_config_flags(build, command: str):  # type of "build" is Build
+    def insert_config_flags(build, command: str) -> str:  # type of "build" is Build
         """Method insert flags in 'command' in line with call of build system
-        or return string with command which run build system with inserted flags"""
+        or return string with command which run build system with inserted flags
+        If 'command' is empty then return string in the format '${BuildSystem} ${config_flags}'
+        else return string 'command' with 'config_flags' inserted"""
 
     @staticmethod
     @abstractmethod
-    def insert_runner_flags(build, command: str):  # type of "build" is Build
+    def insert_runner_flags(build, command: str) -> str:  # type of "build" is Build
         """Method insert flags in 'command' in line with call of runner
-        or return string with command which run runner with inserted flags"""
+        or return string with command which run runner with inserted flags
+        If 'command' is empty then return string in the format '${BuildSystem} ${runner_flags}'
+        else return string 'command' with 'runner_flags' inserted"""
 
 
 class Build:
