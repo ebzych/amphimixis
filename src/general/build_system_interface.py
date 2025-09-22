1ee3c5c73458a7c47d871e744f6a1f030d261d3e
ebzych <bzychkin.egor@gmail.com>
2025-09-22 19:54:41 +0300

    refactor: rename definitions.py to build_system_interface.py and separate CMake implementation, IBuildSystem and general.py

---
+++ src/general/build_system_interface.py
@@ -0,0 +1,28 @@
+"""Definition of interface for build systems"""
+
+from abc import ABC, abstractmethod
+from general import Build, Project
+
+
+class IBuildSystem(ABC):
+    """Interface for classes implementing interaction with build system"""
+
+    @staticmethod
+    @abstractmethod
+    def insert_config_flags(
+        project: Project, build: Build, command: str
+    ) -> str:  # type of "build" is Build, type of "project" is Project
+        """Method insert flags in 'command' in line with call of build system
+        or return string with command which run build system with inserted flags
+        If 'command' is empty then return string in the format '${BuildSystem} ${config_flags}'
+        else return string 'command' with 'config_flags' inserted"""
+
+    @staticmethod
+    @abstractmethod
+    def insert_runner_flags(
+        project: Project, build: Build, command: str
+    ) -> str:  # type of "build" is Build, type of "project" is Project
+        """Method insert flags in 'command' in line with call of runner
+        or return string with command which run runner with inserted flags
+        If 'command' is empty then return string in the format '${BuildSystem} ${runner_flags}'
+        else return string 'command' with 'runner_flags' inserted"""
