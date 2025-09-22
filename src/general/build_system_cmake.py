909b853ddf52381cc5f25148963d1a5cf32a4309
ebzych <bzychkin.egor@gmail.com>
2025-09-22 19:54:41 +0300

    refactor: rename definitions.py to build_system_interface.py and separate CMake implementation, IBuildSystem and general.py

--- src/general/build_system_cmake.py
+++ src/general/build_system_cmake.py
@@ -1,9 +1,18 @@
-from abc import ABC, abstractmethod
+"""CMake implementaion of IBuildSystem"""
+
+from abc import abstractmethod
 from general import Build, Project
+from .build_system_interface import IBuildSystem
 
 
-class IBuildSystem(ABC):
-    """Interface for classes implementing interaction with build system"""
+class CMake(IBuildSystem):
+    """The CMake implementation of IBuildSystem"""
+
+    @staticmethod
+    def find_cmakelists_path(project: Project):
+        """The method find CMakeLists.txt file"""
+
+        raise NotImplementedError
 
     @staticmethod
     @abstractmethod
@@ -15,6 +24,16 @@ class IBuildSystem(ABC):
         If 'command' is empty then return string in the format '${BuildSystem} ${config_flags}'
         else return string 'command' with 'config_flags' inserted"""
 
+        if command != "":
+            raise NotImplementedError
+
+        command = "cmake " + CMake.find_cmakelists_path(project)
+        command += " " + build.config_flags + " "
+        command += ' CXXFLAGS="' + build.compiler_flags + '"'
+        command += ' CFLAGS="' + build.compiler_flags
+        command += " COMPILER=" + build.arch.compiler()
+        return command
+
     @staticmethod
     @abstractmethod
     def insert_runner_flags(
@@ -24,3 +43,5 @@ class IBuildSystem(ABC):
         or return string with command which run runner with inserted flags
         If 'command' is empty then return string in the format '${BuildSystem} ${runner_flags}'
         else return string 'command' with 'runner_flags' inserted"""
+
+        raise NotImplementedError
