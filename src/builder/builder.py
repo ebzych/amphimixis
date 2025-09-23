44cc005ff9874760d88364f4dd64cf5750270e54
ebzych <bzychkin.egor@gmail.com>
2025-09-23 03:20:40 +0300

    fix: building functions are receiving argument of type Project now

--- src/builder/builder.py
+++ src/builder/builder.py
@@ -14,18 +14,18 @@ class Builder:
 
         for build in project.builds:
             if build.is_specified_script:
-                Builder.build_with_specified_script(build)
+                Builder.build_with_specified_script(project, build)
             else:
-                Builder.build_for_linux(build)
+                Builder.build_for_linux(project, build)
 
     @staticmethod
-    def build_for_linux(build: Build):
+    def build_for_linux(project: Project, build: Build):
         """The method build program on Linux"""
 
         command = "mkdir " + build.build_path
         command += " && cd " + build.build_path
-        command += " && cmake" + build.runner.insert_config_flags(build, "")
-        command += " && make" + build.runner.insert_runner_flags(build, "")
+        command += " && cmake" + build.runner.insert_config_flags(project, build, "")
+        command += " && make" + build.runner.insert_runner_flags(project, build, "")
         # make install
         args = shlex.split(command)
         subprocess.Popen(args)
@@ -33,9 +33,9 @@ class Builder:
         # windws trubles
 
     @staticmethod
-    def build_with_specified_script(build: Build):
+    def build_with_specified_script(project: Project, build: Build):
         """The method handle case when user specify a script for building"""
 
         command = build.specified_script
-        command = build.build_system.insert_config_flags(build, command)
-        command = build.runner.insert_runner_flags(build, command)
+        command = build.build_system.insert_config_flags(project, build, command)
+        command = build.runner.insert_runner_flags(project, build, command)
