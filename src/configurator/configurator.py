5f15fa47072ba8ac123817fc30bd91fcd2951192
Michael Mishin <kanalpoigrammisha@gmail.com>
2025-09-27 20:11:16 +0300

    feat(configurator): add script support and custom JSON encoder support, small fixes. Works properly now

--- src/configurator/configurator.py
+++ src/configurator/configurator.py
@@ -1,40 +1,55 @@
 """Module for configuring a new build"""
 
-import os
+from os import path, getcwd
 import json
 import general
 
 
-def configure(
-    project: general.Project,
-    build_path: str,
-    machine: general.MachineInfo,
-    is_specified_script: bool,
-    specified_script: str,
-    build_system: general.IBuildSystem,
-    config_flags: str,
-    compiler_flags: str,
-    runner: general.IBuildSystem,
-):
+class _ComplexEncoder(json.JSONEncoder):
+    def default(self, o):
+        if hasattr(o, "reprJSON"):
+            return o.reprJSON()
+        else:
+            return json.JSONEncoder.default(self, o)
+
+
+def configure(project: general.Project, build_dict: dict[str, str]):
     """Function to configure a new build and save its configuration to a JSON file"""
 
-    project.build_system = build_system
-    project.runner = runner
+    build_path = generate_build_path(project)
 
-    build_name = input("Name current build:\n")
-    build_path = os.path.join(build_path, build_name)
-    os.makedirs(build_path, exist_ok=True)
+    auth = general.MachineAuthenticationInfo(
+        build_dict["username"], build_dict["password"], int(build_dict["port"])
+    )
 
-    build = general.Build(machine, build_path)
-    build.is_specified_script = is_specified_script
-    build.specified_script = specified_script
-    build.config_flags = config_flags
-    build.compiler_flags = compiler_flags
+    machine = general.MachineInfo(
+        general.Arch(build_dict["arch"].lower()), build_dict["ip"], auth
+    )
+    
+    if "script" in build_dict:
+        build = general.Build(machine, build_path, True, build_dict["script"])
+    else:
+        build = general.Build(machine, build_path, False)
+        
+    build.config_flags = build_dict["config_flags"]
+    build.compiler_flags =  build_dict["compiler_flags"]
 
     project.builds.append(build)
 
     config = build.__dict__
 
-    config_path = os.path.join(build_path, "config.json")
-    with open(config_path, "w", encoding="UTF-8") as file:
-        json.dump(config, file, indent=4)
+    config_name = f"{project.build_system}_{project.runner}.json"
+    with open(config_name, "w", encoding="UTF-8") as file:
+        json.dump(config, file, indent=4, cls=_ComplexEncoder)
+
+
+def generate_build_path(project: general.Project):
+    return path.join(getcwd(), f"{project.build_system}_{project.runner}")
+
+
+def parse_config(project: general.Project):
+    project.builds = []
+    with open("input.json", "r", encoding="UTF-8") as f:
+        input_config = json.load(f)
+        for build in input_config["builds"]:
+            configure(project, build)
