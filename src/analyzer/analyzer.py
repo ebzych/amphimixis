0c9e607d45fb6c2119dd8a144cdc1efe0bf39dec
Julia <juliaadod@gmail.com>
2025-09-30 02:38:54 +0900

    refactor(analyzer): extract build_systems from function

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -8,6 +8,14 @@ import json
 class Analyzer:
     """Class that analyzes project's repository and creates file with its information"""
 
+    build_systems_list = {
+        "**/CMakeLists.txt": "cmake",
+        "**/*.cmake": "cmake",
+        "**/configure.ac": "autoconf",
+        "**/*meson": "meson",
+        "**/*bazel": "bazel",
+    }
+
     def __init__(self, repo_path: str):
         self.repo_path = repo_path
         self.results = {
@@ -41,20 +49,9 @@ class Analyzer:
             self.results["ci"] = True
 
     def _search_build_systems(self):
-        if glob.glob(os.path.join(self.repo_path, "**/CMakeLists.txt"), recursive=True):
-            self.results["build_systems"]["cmake"] = True
-
-        if glob.glob(os.path.join(self.repo_path, "**/*.cmake"), recursive=True):
-            self.results["build_systems"]["cmake"] = True
-
-        if glob.glob(os.path.join(self.repo_path, "**/configure.ac"), recursive=True):
-            self.results["build_systems"]["autoconf"] = True
-
-        if glob.glob(os.path.join(self.repo_path, "**/*meson"), recursive=True):
-            self.results["build_systems"]["meson"] = True
-
-        if glob.glob(os.path.join(self.repo_path, "**/*bazel"), recursive=True):
-            self.results["build_systems"]["bazel"] = True
+        for pattern, system in self.build_systems_list.items():
+            if glob.glob(os.path.join(self.repo_path, pattern), recursive=True):
+                self.results["build_systems"][system] = True
 
     def _search_dependencies(self):
         dep_path = os.path.join(self.repo_path, "third_party")
@@ -80,29 +77,3 @@ class Analyzer:
 
         with open("amphimixis.log", "w", encoding="utf8") as file:
             json.dump(self.results, file, indent=4)
-
-            # for key, value in results.items():
-            #     if key == "build_systems":
-            #         print("build_systems:")
-            #         for key2, value2 in results["build_systems"].items():
-            #             if value2:
-            #                 json.dump(f"\t-{key2}\n", file)
-            #                 print("\t-", key2)
-            #     elif key == "dependencies":
-            #         if value:
-            #             file.write(f"{key}:\n")
-            #             print(f"{key}:")
-            #             for dep in value:
-            #                 json.dump(f"\t-{dep}\n", file)
-            #                 print("\t-", dep)
-            #         else:
-            #             json.dump(f"{key}: could not find\n", file)
-            #             print(f"{key}: could not find")
-            #     else:
-            #         line = f"{key}: {'found' if value else 'could not find'}"
-            #         json.dump(line + "\n", file)
-            #         print(line)
-
-    # def search(self, pattern, result):
-    #   if glob.glob(os.path.join(self.repo_path, {pattern}), recursive=True):
-    #       self.results[{result}] = True
