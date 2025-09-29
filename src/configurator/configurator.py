c206fae109014968101247fc92cb70ef075a7a26
Michael Mishin <kanalpoigrammisha@gmail.com>
2025-09-29 19:28:55 +0300

    refactor(configurator): changed serialization format to pickle

--- src/configurator/configurator.py
+++ src/configurator/configurator.py
@@ -1,19 +1,12 @@
 """Module for configuring a new build"""
 
 from os import path, getcwd
+import pickle
 import json
 import general
 
 
-class _ComplexEncoder(json.JSONEncoder):
-    def default(self, o):
-        if hasattr(o, "reprJSON"):
-            return o.reprJSON()
-        else:
-            return json.JSONEncoder.default(self, o)
-
-
-def configure(project: general.Project, build_dict: dict[str, str]):
+def configure(project: general.Project, build_dict: dict[str, str]) -> None:
     """Function to configure a new build and save its configuration to a JSON file"""
 
     build_path = generate_build_path(project)
@@ -25,29 +18,31 @@ def configure(project: general.Project, build_dict: dict[str, str]):
     machine = general.MachineInfo(
         general.Arch(build_dict["arch"].lower()), build_dict["ip"], auth
     )
-    
+
     if "script" in build_dict:
         build = general.Build(machine, build_path, True, build_dict["script"])
     else:
         build = general.Build(machine, build_path, False)
-        
+
     build.config_flags = build_dict["config_flags"]
-    build.compiler_flags =  build_dict["compiler_flags"]
+    build.compiler_flags = build_dict["compiler_flags"]
 
     project.builds.append(build)
 
     config = build.__dict__
 
-    config_name = f"{project.build_system}_{project.runner}.json"
-    with open(config_name, "w", encoding="UTF-8") as file:
-        json.dump(config, file, indent=4, cls=_ComplexEncoder)
+    config_name = f"{project.build_system}_{project.runner}"
+    with open(config_name, "wb") as file:
+        pickle.dump(config, file)
 
 
-def generate_build_path(project: general.Project):
+def generate_build_path(project: general.Project) -> str:
+    """Function to create path to build"""
     return path.join(getcwd(), f"{project.build_system}_{project.runner}")
 
 
-def parse_config(project: general.Project):
+def parse_config(project: general.Project) -> None:
+    """Module enter function"""
     project.builds = []
     with open("input.json", "r", encoding="UTF-8") as f:
         input_config = json.load(f)
