852269f8e5c66b5b4f7ec16aa84b391ae0d7a19e
Dmitrii Bordukov <dabordukov@mail.ru>
2025-10-12 00:43:49 +0300

    refactor(shell): Reduced a number of parameters
    Shell constructor now accepts one MachineInfo parameter, which contains all needed data.

--- src/shell/shell.py
+++ src/shell/shell.py
@@ -1,31 +1,40 @@
 """Module for shell operations."""
 
 import ipaddress
+from ctypes import ArgumentError
 from typing import Self
 
+from general import MachineInfo
+
+from .local_shell_handler import _LocalShellHandler
 from .shell_interface import IShellHandler
 from .ssh_shell_handler import _SSHHandler
-from .local_shell_handler import _LocalShellHandler
 
 
 class Shell:
     """Shell class to manage shell operations."""
 
-    def __init__(self, ip: str, port: str, username: str, password: str):
+    def __init__(self, machine: MachineInfo):
         self._shell: IShellHandler
-        self.username = username
-        self.password = password
-        self.ip = ipaddress.ip_address(ip)
-        self.port = port
+        self.machine = machine
 
     def connect(self) -> Self:
         """Connect to the shell based on the IP address."""
-        if self.ip.is_loopback:
+
+        if self.machine.ip is None:
             self._shell = _LocalShellHandler()
-        else:
+            return self
+
+        ip = ipaddress.ip_address(self.machine.ip)
+        if ip.is_loopback:
             self._shell = _SSHHandler(
-                str(self.ip), int(self.port), self.username, self.password
+                str(ip),
+                int(self.machine.auth.port),
+                self.machine.auth.username,
+                self.machine.auth.password,
             )
+        else:
+            raise ArgumentError(f"{self.machine.ip} is loopback address.")
 
         return self
 
