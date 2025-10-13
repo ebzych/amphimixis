d8af9fbff4a7a9a6287aa76057352b909fd5fae6
Michael Mishin <88842044+IMBIGFISH4@users.noreply.github.com>
2025-10-14 01:50:42 +0300

    Configurator - split platform into build, run machines. Add sysroot, toolchain support  (#15)
    * refactor(configurator): add project path check and make configurator up-to-date
    
    * chore(configurator): remove excess files
    
    * reafactor(configurator): add successful completion message
    
    * refactor(configurator): add input file open check
    
    * refactor(configurator): add exception raise at invalid receipt/platform id
    
    * refactor(configurator): changed error messages
    
    * refactor(configurator, general): add sysroot and toolchain support, split platform in build into run and build machines
    
    * Delete input.yml

--- src/configurator/configurator.py
+++ src/configurator/configurator.py
@@ -9,51 +9,86 @@ import general
 def parse_config(project: general.Project) -> None:
     """Module enter function"""
 
+    if not path.exists(project.path):
+        raise FileNotFoundError("Incorrect project path @_@, check input arguments")
+
     project.builds = []
-    with open("input.yml", "r", encoding="UTF-8") as f:
-        input_config = yaml.safe_load(f)
+    try:
+        with open("input.yml", "r", encoding="UTF-8") as f:
+            input_config = yaml.safe_load(f)
 
-        if not isinstance(input_config["build_system"], str):
-            raise TypeError("Invalid build system type :(, look config file")
+            if not isinstance(input_config["build_system"], str):
+                raise TypeError("Invalid build system type :(, check config file")
+            project.build_system = general.build_systems_dict[
+                input_config["build_system"].lower()
+            ]
 
-        project.build_system = general.build_systems_dict[
-            input_config["build_system"].lower()
-        ]
+            if not isinstance(input_config["runner"], str):
+                raise TypeError("Invalid runner type ^~^, check config file")
+            project.runner = general.build_systems_dict[input_config["runner"].lower()]
 
-        if not isinstance(input_config["runner"], str):
-            raise TypeError("Invalid runner type ^~^, look config file")
+            for build in input_config["builds"]:
+                toolchain = None
+                sysroot = None
 
-        project.runner = general.build_systems_dict[input_config["runner"].lower()]
+                if "toolchain" in build:
+                    if not isinstance(build["toolchain"], str):
+                        raise TypeError("Invalid toolchain type, check config file")
+                    toolchain = build["toolchain"]
 
-        for build in input_config["builds"]:
-            configure(
-                project,
-                get_by_id(input_config["platforms"], build["platform_id"]),
-                get_by_id(input_config["receipts"], build["receipt_id"]),
-            )
+                if "sysroot" in build:
+                    if not isinstance(build["sysroot"], str):
+                        raise TypeError("Invalid sysroot type, check config file")
+                    sysroot = build["sysroot"]
+
+                configure(
+                    project,
+                    get_by_id(input_config["platforms"], build["build_machine"]),
+                    get_by_id(input_config["platforms"], build["run_machine"]),
+                    get_by_id(input_config["receipts"], build["receipt_id"]),
+                    toolchain,
+                    sysroot,
+                )
+
+    except FileNotFoundError as e:
+        print(f"Error opening file, check input data. {e}")
+
+    print("Configuration completed successfully!")
 
 
 def configure(
     project: general.Project,
-    platform_info: dict[str, str],
+    build_machine_info: dict[str, str],
+    run_machine_info: dict[str, str],
     receipt_info: dict[str, str],
+    toolchain: str | None,
+    sysroot: str | None,
 ) -> None:
     """Function to configure a new build and save its configuration to a Pickle file"""
 
-    build_path = generate_build_path(platform_info["id"], receipt_info["id"])
-
-    auth = general.MachineAuthenticationInfo(
-        platform_info["username"], platform_info["password"], int(platform_info["port"])
+    build_path = generate_build_path(
+        build_machine_info["id"], run_machine_info["id"], receipt_info["id"]
     )
 
-    machine = general.MachineInfo(
-        general.Arch(platform_info["arch"].lower()), platform_info["ip"], auth
-    )
+    build_machine = create_machine(build_machine_info)
+    run_machine = create_machine(run_machine_info)
 
     if "script" in receipt_info:
-        build = general.Build(machine, build_path, True, receipt_info["script"])
+        if not isinstance(receipt_info["script"], str):
+            raise TypeError("Invalid path to script type, check config file")
+        build = general.Build(
+            build_machine,
+            run_machine,
+            build_path,
+            toolchain,
+            sysroot,
+            True,
+            receipt_info["script"],
+        )
     else:
-        build = general.Build(machine, build_path, False)
+        build = general.Build(
+            build_machine, run_machine, build_path, toolchain, sysroot, False
+        )
 
     build.config_flags = receipt_info["config_flags"]
     build.compiler_flags = receipt_info["compiler_flags"]
@@ -62,15 +97,60 @@ def configure(
 
     config = build.__dict__
 
-    config_name = f"{platform_info['id']}_{receipt_info['id']}_config"
+    config_name = f"{build_path}_config"
     with open(config_name, "wb") as file:
         pickle.dump(config, file)
 
 
-def generate_build_path(platform_id: str, receipt_id: str) -> str:
-    """Function to create path to build depending on platform and receipt id"""
+def create_machine(machine_info: dict[str, str]) -> general.MachineInfo:
+    """Function to create a new machine"""
 
-    return path.join(getcwd(), f"{platform_id}_{receipt_id}")
+    if not isinstance(machine_info["arch"], str):
+        raise TypeError("Invalid arch type, check config file")
+
+    if "address" in machine_info:
+        if not isinstance(machine_info["address"], str):
+            raise TypeError("Invalid address type, check config file")
+
+        if "username" not in machine_info or not isinstance(
+            machine_info["username"], str
+        ):
+            raise TypeError(
+                "Invalid username type or username does not exist, check config file"
+            )
+
+        address = machine_info["address"]
+        username = machine_info["username"]
+        password = None
+        port = 22
+
+        if "password" in machine_info:
+            if not isinstance(machine_info["password"], str):
+                raise TypeError("Invalid password type, check config file")
+            password = machine_info["password"]
+
+        if "port" in machine_info:
+            if not isinstance(machine_info["port"], int):
+                raise TypeError("Invalid port type, check config file")
+            port = machine_info["port"]
+
+        auth = general.MachineAuthenticationInfo(username, password, port)
+
+        machine = general.MachineInfo(
+            general.Arch(machine_info["arch"].lower()), address, auth
+        )
+    else:
+        machine = general.MachineInfo(
+            general.Arch(machine_info["arch"].lower()), None, None
+        )
+
+    return machine
+
+
+def generate_build_path(build_id: str, run_id: str, receipt_id: str) -> str:
+    """Function to create path to build, depending on build, run and receipt ids"""
+
+    return path.join(getcwd(), f"{build_id}_{run_id}_{receipt_id}")
 
 
 def get_by_id(items: list[dict[str, str]], target_id: str) -> dict[str, str]:
@@ -79,4 +159,5 @@ def get_by_id(items: list[dict[str, str]], target_id: str) -> dict[str, str]:
     for item in items:
         if item["id"] == target_id:
             return item
-    return {}
+
+    raise LookupError("Item id didn't match any existed id, check input file")
