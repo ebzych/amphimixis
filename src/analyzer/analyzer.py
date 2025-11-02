5b6e45ed240a2aacbf5c1e470dd987e4d770cc7e
Julia <juliaadod@gmail.com>
2025-11-02 22:28:14 +0300

    refactor(analyzer): change results from global to local variable

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -3,6 +3,7 @@
 import glob
 import json
 from os import path, listdir
+import sys
 import general
 from general import Colors
 
@@ -17,22 +18,22 @@ build_systems_list = {
     "**/*bazel": "bazel",
 }
 
-results = {
-    "tests": [],
-    "benchmarks": [],
-    "ci": [],
-    "build_systems": {
-        "cmake": False,
-        "autoconf": False,
-        "meson": False,
-        "bazel": False,
-    },
-    "dependencies": [],
-}
-
 
 def analyze(project: general.Project):
     """Analyzes project and collects its information"""
+
+    results = {
+        "tests": [],
+        "benchmarks": [],
+        "ci": [],
+        "build_systems": {
+            "cmake": False,
+            "autoconf": False,
+            "meson": False,
+            "bazel": False,
+        },
+        "dependencies": [],
+    }
 
     proj_path = project.path
 
@@ -43,11 +44,11 @@ def analyze(project: general.Project):
 
         print(f"Analyzing {path.basename(path.normpath(proj_path))}\n" + "\n")
 
-        _search_tests(proj_path)
-        _search_benchmarks(proj_path)
-        _search_ci(proj_path)
-        _search_build_systems(proj_path)
-        _search_dependencies(proj_path)
+        _search_tests(proj_path, results)
+        _search_benchmarks(proj_path, results)
+        _search_ci(proj_path, results)
+        _search_build_systems(proj_path, results)
+        _search_dependencies(proj_path, results)
 
         print("\nAnalyzing done\n")
 
@@ -57,7 +58,7 @@ def analyze(project: general.Project):
 
     except FileNotFoundError as e:
         print(f"{e}")
-        exit(-1)
+        sys.exit(-1)
 
 
 def _rel_path(proj_path, paths):
@@ -70,7 +71,7 @@ def _find_paths(proj_path, pattern, dirs_only=True):
     return [p for p in paths if path.isdir(p)] if dirs_only else paths
 
 
-def _path_existence(proj_path, key, paths):
+def _path_existence(proj_path, results, key, paths):
     if paths:
         results[f"{key}"] = _rel_path(proj_path, paths)
 
@@ -83,31 +84,27 @@ def _path_output(proj_path, key, paths):
         print(f"{key}:".ljust(_TAB) + Colors.RED + "not found\n" + Colors.NONE)
 
 
-def _search_tests(proj_path):
+def _search_tests(proj_path, results):
     paths = _find_paths(proj_path, "**/*test*")
-    _path_existence(proj_path, "tests", paths)
+    _path_existence(proj_path, results, "tests", paths)
     _path_output(proj_path, "tests", paths)
 
 
-def _search_benchmarks(proj_path):
+def _search_benchmarks(proj_path, results):
     paths = _find_paths(proj_path, "**/*benchmark*")
-    _path_existence(proj_path, "benchmarks", paths)
+    _path_existence(proj_path, results, "benchmarks", paths)
     _path_output(proj_path, "benchmarks", paths)
 
 
-def _search_ci(
-    proj_path,
-):
+def _search_ci(proj_path, results):
     paths = []
     for pattern in ci_list:
         paths.extend(_find_paths(proj_path, pattern))
-    _path_existence(proj_path, "ci", paths)
+    _path_existence(proj_path, results, "ci", paths)
     _path_output(proj_path, "ci", paths)
 
 
-def _search_build_systems(
-    proj_path,
-):
+def _search_build_systems(proj_path, results):
     print("build systems:")
     for pattern, system in build_systems_list.items():
         if _find_paths(proj_path, pattern, dirs_only=False):
@@ -118,7 +115,7 @@ def _search_build_systems(
     print()
 
 
-def _search_dependencies(proj_path):
+def _search_dependencies(proj_path, results):
     dep_path = path.join(proj_path, "third_party")
     if path.exists(dep_path):
         dirs = [d for d in listdir(dep_path) if path.isdir(path.join(dep_path, d))]
