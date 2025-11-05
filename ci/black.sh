73587077899325257abd5890f95512630e19b627
maria <novikmaria36@gmail.com>
2025-11-05 11:31:22 +0300

    fix(ci): remove poetry support, use uv only

--- ci/black.sh
+++ ci/black.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 BLUE='\e[34m'
 NC='\e[0m'
-COMMAND="${1}"
 
 echo -e "${BLUE}Running Black code formatter...${NC}"
 mapfile -t files < <(git ls-files '*.py')
-"$COMMAND" run black --check "${files[@]}"
+uv run black --check "${files[@]}"
 
 
--- ci/runner.sh
+++ ci/runner.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 set -e
 
-RED='\e[31m'
 BLUE='\e[34m'
 NC='\e[0m'
 
 echo "Running shellcheck..."
 shellcheck ci/*.sh
 
-COMMAND=$(./ci/environment_configure.sh)
+./ci/environment_configure.sh
 
-if [ -n "$COMMAND" ]; then
-    echo -e "${BLUE}Run CI:${NC}"
-    ./ci/black.sh "$COMMAND"
-    ./ci/mypy.sh "$COMMAND" 
-    ./ci/pylint.sh "$COMMAND"
-    ./ci/ruff.sh "$COMMAND"
-    
-else
-    echo -e "${RED}Error: CI can not run ${NC}"
-    exit 1
-fi
+echo -e "${BLUE}Run CI:${NC}"
+./ci/black.sh
+./ci/mypy.sh
+./ci/pylint.sh
+./ci/ruff.sh
