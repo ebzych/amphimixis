8d03b48698c25e41f621e6b672fd69eb0d394060
ebzych <bzychkin.egor@gmail.com>
2025-11-06 22:16:13 +0300

    fix: fix imports, add public interfaces, add docstrings, disable some ruff warnings in code

---
+++ tests/test_toolchain_manager/__init__.py
@@ -0,0 +1,4 @@
+"""Tests of ToolchainManager"""
+
+from tests.test_toolchain_manager.test_parsing_config_file import *  # noqa: F403
+from tests.test_toolchain_manager.test_add_toolchain_and_sysroot import *  # noqa: F403
