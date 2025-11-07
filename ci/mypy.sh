c8fdffbe391b7de82ef999d8d1c20297bd444a47
maria <novikmaria36@gmail.com>
2025-11-07 14:51:20 +0300

    fix(ci): replace relative paths with absolute ones

--- ci/mypy.sh
+++ ci/mypy.sh
@@ -3,6 +3,6 @@ BLUE='\e[34m'
 NC='\e[0m'
 
 echo -e "${BLUE}Running MyPy type checker...${NC}"
-mapfile -t files < <(git ls-files '*.py')
+mapfile -t files < <(git ls-files ':/*.py')
 uv run mypy "${files[@]}"
 
