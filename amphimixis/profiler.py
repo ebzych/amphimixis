ea7841bbc6b409f8fb05f8afff8c6623586bf26a
Dmitrii Bordukov <dabordukov@mail.ru>
2025-11-16 18:11:00 +0300

    feat(profiler): implement execution time and perf stat collection
    - Add Stats enum for profiler stats fields
    - Implement execution time measurement using /bin/time
    - Implement perf stat collection
    - Update Profiler class to use shell and machine information
    - Add test_executable method to check if executable runs without errors

--- amphimixis/profiler.py
+++ amphimixis/profiler.py
@@ -1,61 +1,90 @@
 """Module for profiling executables within a project."""
 
-import os
-import subprocess
-import time
-from amphimixis import general
+from enum import Enum
 
-STATS_EXECUTION_TIME_FIELD = "execution_time"
+from amphimixis import general, logger, shell
 
 
-def _executable_choose(files):
-    return files[0]
+class Stats(Enum):
+    """Profiler stats fields"""
 
-
-def _choose_executable(build: general.Build) -> str:
-    executables = []
-    files = os.listdir(os.path.join(build.build_path))
-    for file in files:
-        if os.path.isdir(os.path.join(build.build_path, file)):
-            continue
-
-        with open(os.path.join(build.build_path, file), "rb") as data:
-            _bytes = data.read(4)
-            if _bytes[:2] == "MZ" or _bytes == b"\x7fELF":
-                executables.append(file)
-
-    return os.path.join(build.build_path, _executable_choose(executables))
+    REAL_TIME = 0
+    USER_TIME = 1
+    KERNEL_TIME = 2
+    EXECUTABLE_RUN_SUCCESS = 3
+    PERF_STAT = 4
 
 
 class Profiler:
     """Class for profiling an executable within a project."""
 
     def __init__(self, build: general.Build, executable: str = ""):
+        self.logger = logger.setup_logger("PROFILER")
+        self.machine = build.run_machine
         self.build = build
-        if executable == "":
-            self.executable = _choose_executable(build)
-        else:
-            self.executable = executable
+        self.executable = executable
+        self.shell = shell.Shell(self.machine).connect()
+        self.stats: dict[Stats, str] = {}
 
-        self.stats: dict[str, str] = {}
+    def execution_time(self) -> bool:
+        """Measure execution time: real, user, kernel"""
 
-    def execution_time(self):
-        """Measure execution time of the executable."""
-        start_time = time.time()
-        with subprocess.Popen(
-            [self.executable],
-            cwd=self.build.build_path,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ) as process:
-            process.wait()
+        error, _, stderr = self.shell.run(f"cd {self.build.build_path}")
+        if error != 0:
+            self.logger.error(" ".join(stderr[0]))
+            return False
 
-        end_time = time.time()
-        self.stats.update({STATS_EXECUTION_TIME_FIELD: str(end_time - start_time)})
+        error, _, stderr = self.shell.run(
+            f'/bin/time -f"%e\n%U\n%S" sh -c "{self.executable}"'
+        )
+        if error != 0:
+            self.logger.error(" ".join(stderr[0]))
+            return False
 
-    def perf_stat_collect(self):
+        self.stats.update(
+            {
+                Stats.REAL_TIME: stderr[0][-3],
+                Stats.USER_TIME: stderr[0][-2],
+                Stats.KERNEL_TIME: stderr[0][-1],
+            }
+        )
+        return True
+
+    def test_executable(self) -> bool:
+        """Checks if executable runs and returns no errors"""
+
+        error, _, _ = self.shell.run(
+            f"cd {self.build.build_path}", f"{self.executable}"
+        )
+
+        self.stats.update(
+            {Stats.EXECUTABLE_RUN_SUCCESS: "true" if error == 0 else "false"}
+        )
+
+        return error == 0
+
+    def perf_stat_collect(self) -> bool:
         """Collect performance statistics using 'perf stat'."""
-        raise NotImplementedError
+
+        error, _, stderr = self.shell.run(
+            f"cd {self.build.build_path}",
+        )
+
+        if error != 0:
+            self.logger.error(" ".join(stderr[0]))
+            return False
+
+        error, _, stderr = self.shell.run(
+            f"perf stat -ddd -x, sh -c '{self.executable} 2>/dev/null'",
+        )
+
+        if error != 0:
+            self.logger.error("Executable returned %d code", error)
+            return False
+
+        self.stats.update({Stats.PERF_STAT: "".join(stderr[0])})
+
+        return True
 
     def perf_record_collect(self):
         """Collect performance records using 'perf record'."""
