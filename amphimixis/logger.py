5b4748f947185664ab9785a0562ecd610f52b17c
Michael Mishin <kanalpoigrammisha@gmail.com>
2025-11-17 20:20:09 +0300

    feat(logger): created logger

---
+++ amphimixis/logger.py
@@ -0,0 +1,20 @@
+"""Module to operate logging"""
+
+import logging
+
+LOG_FILE_NAME = "amphimixis.log"
+
+
+def setup_logger(name: str) -> logging.Logger:
+    """Function to create logger with specified name"""
+
+    if not logging.getLogger().hasHandlers():
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+            handlers=[
+                logging.FileHandler(LOG_FILE_NAME, encoding="utf-8"),
+            ],
+        )
+
+    return logging.getLogger(name)
