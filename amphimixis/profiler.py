9ce46f935372cc260d9db083d0da47db58c049fc
Dmitrii Bordukov <dabordukov@mail.ru>
2025-11-19 22:06:28 +0300

    fix(profiler): enhance error logging and improve command execution for performance stats

--- amphimixis/profiler.py
+++ amphimixis/profiler.py
@@ -34,11 +34,15 @@ class Profiler:
             self.logger.error(" ".join(stderr[0]))
             return False
 
-        error, _, stderr = self.shell.run(
-            f'/bin/time -f"%e\n%U\n%S" sh -c "{self.executable}"'
+        command = f'/bin/time -f"%e\n%U\n%S" taskset -c 0 sh -c "./{self.executable}"'
+        self.logger.info(
+            "Measure execution time %s\n\tCommand: %s", self.executable, command
         )
+        error, stdout, stderr = self.shell.run(command)
         if error != 0:
-            self.logger.error(" ".join(stderr[0]))
+            error_message = "STDERR: " + "".join(stderr[0])
+            error_message += "STDOUT: " + "".join(stdout[0])
+            self.logger.error(error_message)
             return False
 
         self.stats.update(
@@ -53,9 +57,11 @@ class Profiler:
     def test_executable(self) -> bool:
         """Checks if executable runs and returns no errors"""
 
-        error, _, _ = self.shell.run(
-            f"cd {self.build.build_path}", f"{self.executable}"
-        )
+        error, stdout, stderr = self.shell.run(f"cd {self}", f"./{self.executable}")
+        if error != 0:
+            error_message = "STDERR: " + "".join(stderr[0])
+            error_message += "STDOUT: " + "".join(stdout[0])
+            self.logger.error(error_message)
 
         self.stats.update(
             {Stats.EXECUTABLE_RUN_SUCCESS: "true" if error == 0 else "false"}
@@ -63,7 +69,7 @@ class Profiler:
 
         return error == 0
 
-    def perf_stat_collect(self) -> bool:
+    def perf_stat_collect(self, options: str = "") -> bool:
         """Collect performance statistics using 'perf stat'."""
 
         error, _, stderr = self.shell.run(
@@ -74,12 +80,14 @@ class Profiler:
             self.logger.error(" ".join(stderr[0]))
             return False
 
-        error, _, stderr = self.shell.run(
-            f"perf stat -ddd -x, sh -c '{self.executable} 2>/dev/null'",
-        )
+        command = f"perf stat {options} -ddd -x, taskset -c 0 sh -c './{self.executable} 2>/dev/null'"
+        self.logger.info("Collecting perfomance counters with:\n\t%s", command)
+        error, _, stderr = self.shell.run(command)
 
         if error != 0:
-            self.logger.error("Executable returned %d code", error)
+            self.logger.error(
+                "Executable returned %d code\n%s", error, "".join(stderr[0])
+            )
             return False
 
         self.stats.update({Stats.PERF_STAT: "".join(stderr[0])})
