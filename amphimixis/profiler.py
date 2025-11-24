e97d90b218194a53d66a5a87c8b347055994a4e1
Dmitrii Bordukov <53707617+dabordukov@users.noreply.github.com>
2025-11-24 23:28:00 +0300

    Profiler update (#34)
    * feat(profiler): implement execution time and perf stat collection
    
    - Add Stats enum for profiler stats fields
    - Implement execution time measurement using /bin/time
    - Implement perf stat collection
    - Add test_executable method to check if executable runs without errors

--- amphimixis/profiler.py
+++ amphimixis/profiler.py
@@ -1,61 +1,115 @@
 """Module for profiling executables within a project."""
 
-import os
-import subprocess
-import time
-from amphimixis import general
+from enum import Enum
 
-STATS_EXECUTION_TIME_FIELD = "execution_time"
+from amphimixis import general, logger, shell
 
 
-def _executable_choose(files):
-    return files[0]
+class Stats(Enum):
+    """Profiler stats fields"""
+
+    REAL_TIME = 0
+    USER_TIME = 1
+    KERNEL_TIME = 2
+    EXECUTABLE_RUN_SUCCESS = 3
+    PERF_STAT = 4
 
 
-def _choose_executable(build: general.Build) -> str:
-    executables = []
-    files = os.listdir(os.path.join(build.build_path))
-    for file in files:
-        if os.path.isdir(os.path.join(build.build_path, file)):
-            continue
-
-        with open(os.path.join(build.build_path, file), "rb") as data:
-            _bytes = data.read(4)
-            if _bytes[:2] == "MZ" or _bytes == b"\x7fELF":
-                executables.append(file)
-
-    return os.path.join(build.build_path, _executable_choose(executables))
+_commands_args: dict[str, dict[str, str]] = {
+    "stat": {
+        "cmd": "perf stat",
+        "opt": "-ddd -x,",
+        "cpu_affinity": "taskset -c 0",
+    },
+    "record": {"cmd": "perf record", "cpu_affinity": "taskset -c 0"},
+    "time": {
+        "cmd": "/bin/time",
+        "format": '-f"%e\\n%U\\n%S"',
+        "cpu_affinity": "taskset -c 0",
+    },
+}
 
 
 class Profiler:
     """Class for profiling an executable within a project."""
 
     def __init__(self, build: general.Build, executable: str = ""):
+        self.logger = logger.setup_logger("PROFILER")
+        self.machine = build.run_machine
         self.build = build
-        if executable == "":
-            self.executable = _choose_executable(build)
-        else:
-            self.executable = executable
+        self.executable = executable
+        self.shell = shell.Shell(self.machine).connect()
+        self.stats: dict[Stats, str] = {}
 
-        self.stats: dict[str, str] = {}
+    def execution_time(self) -> bool:
+        """Measure execution time: real, user, kernel"""
 
-    def execution_time(self):
-        """Measure execution time of the executable."""
-        start_time = time.time()
-        with subprocess.Popen(
-            [self.executable],
-            cwd=self.build.build_path,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ) as process:
-            process.wait()
+        error, _, stderr = self.shell.run(f"cd {self.build.build_path}")
+        if error != 0:
+            self.logger.error("".join(stderr[0]))
+            return False
 
-        end_time = time.time()
-        self.stats.update({STATS_EXECUTION_TIME_FIELD: str(end_time - start_time)})
+        command = self._command("time", stderr_clear=False)
+        self.logger.info(
+            "Measure execution time %s\n\tCommand: %s", self.executable, command
+        )
+        error, stdout, stderr = self.shell.run(command)
+        if error != 0:
+            error_message = "STDERR: " + "".join(stderr[0])
+            error_message += "STDOUT: " + "".join(stdout[0])
+            self.logger.error(error_message)
+            return False
 
-    def perf_stat_collect(self):
+        self.stats.update(
+            {
+                Stats.REAL_TIME: stderr[0][-3],
+                Stats.USER_TIME: stderr[0][-2],
+                Stats.KERNEL_TIME: stderr[0][-1],
+            }
+        )
+        return True
+
+    def test_executable(self) -> bool:
+        """Checks if executable runs and returns no errors"""
+
+        error, stdout, stderr = self.shell.run(f"cd {self.build.build_path}")
+        if error != 0:
+            error_message = "STDERR: " + "".join(line for cmd in stdout for line in cmd)
+            error_message += "STDOUT: " + "".join(
+                line for cmd in stderr for line in cmd
+            )
+            self.logger.error(error_message)
+
+        self.stats.update(
+            {Stats.EXECUTABLE_RUN_SUCCESS: "true" if error == 0 else "false"}
+        )
+
+        return error == 0
+
+    def perf_stat_collect(self, options: str = "") -> bool:
         """Collect performance statistics using 'perf stat'."""
-        raise NotImplementedError
+
+        error, _, stderr = self.shell.run(
+            f"cd {self.build.build_path}",
+        )
+
+        if error != 0:
+            self.logger.error("".join(stderr[0]))
+            return False
+
+        command = self._command("stat", options)
+        self.logger.info("Collecting perfomance counters with:\n\t%s", command)
+        error, _, stderr = self.shell.run(command)
+
+        if error != 0:
+            self.logger.error(
+                "Executable returned %d code\n%s", error, "".join(stderr[0])
+            )
+            return False
+
+        self.stats.update({Stats.PERF_STAT: "".join(stderr[0])})
+
+        return True
 
     def perf_record_collect(self):
         """Collect performance records using 'perf record'."""
@@ -64,6 +118,35 @@ class Profiler:
     def save_stats(self):
         """Save collected statistics to a file."""
         raise NotImplementedError
+
+    def _command(
+        self,
+        module: str,
+        options: str = "",
+        stdout_clear: bool = False,
+        stderr_clear: bool = True,
+    ) -> str:
+        if module not in _commands_args:
+            return ""
+
+        command: list[str] = []
+        command.append(_commands_args[module]["cmd"])
+        if options:
+            command.append(options)
+        for arg in _commands_args[module]:
+            if arg != "cmd":
+                command.append(_commands_args[module][arg])
+
+        command.append(f"sh -c './{self.executable}")
+        if stdout_clear:
+            command.append("1>/dev/null")
+
+        if stderr_clear:
+            command.append("2>/dev/null")
+
+        command.append("'")
+
+        return " ".join(command)
 
 
 if __name__ == "__main__":
