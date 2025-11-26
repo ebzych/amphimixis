4f2efba11b3ded27e92159adb74f39d4cb350991
Valeria <curikovalera3@gmail.com>
2025-11-27 02:54:02 +0300

    feat(cli): add flag --config, change parameters configurator, validator and flag --configure can get path to config file

--- amixis.py
+++ amixis.py
@@ -5,8 +5,17 @@
 import argparse
 import sys
 import textwrap
+from pathlib import Path
 
-from amphimixis import Builder, Profiler, analyze, general, parse_config, validate
+from amphimixis import (
+    Builder,
+    Profiler,
+    analyze,
+    build_systems_dict,
+    general,
+    parse_config,
+    validate,
+)
 
 YELLOW = "\033[93m"
 GRAY = "\033[90m"
@@ -47,9 +56,13 @@ class CustomFormatterClass(
               amixis --build /path/to/folder/with/project
                   → Builds the project, implicitly calling --configure to generate.
                   configuration files.
+                  
+              amixis --configure=config_file /path/to/folder/with/project
+                    → Generates configuration files for various builds based on the provided
+                    config file. If no config file is specified, defaults to 'input.yml', is used by default, which must be located in the working directory.
 
               amixis --validate file_name
-                  → checks the config file correctness.
+                  → Checks the config file correctness.
             """
         )
 
@@ -66,11 +79,11 @@ def main():
         add_help=True,
     )
 
+    default_config_path = Path("input.yml").resolve()
+
     parser.add_argument(
         "path",
         type=str,
-        nargs="?",
-        metavar="path",
         help="path to the project folder to process (required in main mode).",
     )
 
@@ -79,6 +92,7 @@ def main():
         "--validate",
         type=str,
         metavar="file_name",
+        default=None,
         help="checks the correctness of the configuration file.",
     )
 
@@ -88,18 +102,32 @@ def main():
         action="store_true",
         help="analyzes the project to detect existing CI systems, tests, build systems, etc.",
     )
+
+    parser.add_argument(
+        "--config",
+        nargs="?",
+        const=str(default_config_path),
+        metavar="config_file",
+        help="specified path to configuration file; if not provided, defaults to 'input.yml'.",
+    )
+
     parser.add_argument(
         "-c",
         "--configure",
-        action="store_true",
-        help="generates configuration files for various builds.",
+        nargs="?",
+        const=str(default_config_path),
+        metavar="config_file",
+        help="generates configuration files for various builds; allows to specify config file;"
+        "if not provided, defaults to 'input.yml'.",
     )
+
     parser.add_argument(
         "-b",
         "--build",
         action="store_true",
         help="builds the project according to the generated configuration files.",
     )
+
     parser.add_argument(
         "-p",
         "--profile",
@@ -109,6 +137,16 @@ def main():
     )
 
     args = parser.parse_args()
+
+    if args.configure is not None:
+        config_file = Path(args.configure).expanduser().resolve()
+    else:
+        config_file = default_config_path
+
+    if args.config is not None:
+        config_file = Path(args.config).expanduser().resolve()
+    else:
+        config_file = default_config_path
 
     if args.validate:
         validate(args.validate)
@@ -127,43 +165,33 @@ def main():
 
     try:
         if not any([args.analyze, args.configure, args.build, args.profile]):
-
             analyze(project)
-
-            parse_config(project)
-
+            parse_config(project, config_file_path=str(config_file))
             Builder.build(project)
-
             profiler_ = Profiler(project.builds[0])
-
             profiler_.execution_time()
-
             print(profiler_.stats)
 
         if args.analyze:
             analyze(project)
 
-        if args.configure:
-            parse_config(project)
+        if args.configure is not None:
+            parse_config(project, config_file_path=str(config_file))
 
         if args.build:
-
-            if not args.configure:
-                parse_config(project)
+            if not args.configure is not None:
+                parse_config(project, config_file_path=str(config_file))
 
             Builder.build(project)
 
         if args.profile:
-
             profiler_ = Profiler(project.builds[0])
-
             profiler_.execution_time()
-
             print(profiler_.stats)
 
         sys.exit(0)
 
-    except (FileNotFoundError, ValueError, RuntimeError) as e:
+    except (FileNotFoundError, ValueError, RuntimeError, LookupError, TypeError) as e:
         print(f"Error: {e}")
         sys.exit(1)
 
