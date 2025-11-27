37fe122544e6de6a8f6ad67009130a13853431c0
maria <novikmaria36@gmail.com>
2025-11-27 03:55:45 +0300

    fix(test): fix error from Pylint in tests

--- tests/integration/test_e2e.py
+++ tests/integration/test_e2e.py
@@ -1,12 +1,17 @@
-import subprocess
+"""Tests for the full launch of the application on a specific project"""
+
 import os
+import subprocess
 from pathlib import Path
-import yaml
+
 import pytest
+import yaml
 
 
 @pytest.mark.integration
 def test_build_local_machine(clone_repo, create_working_space):
+    """This integration test checks whether the application can successfully build the project
+    on the local machine"""
     repo_path = clone_repo("https://github.com/jbeder/yaml-cpp.git")
     working_dir = create_working_space()
     orig_dir = Path.cwd()
@@ -22,7 +27,7 @@ def test_build_local_machine(clone_repo, create_working_space):
         with open(config_file, "w", encoding="utf-8") as file:
             yaml.dump(config_content, file)
         command = ["python3", orig_dir / "amixis.py", str(repo_path)]
-        result = subprocess.run(command)
+        result = subprocess.run(command, check=True)
 
         assert result.returncode == 0
 
