eff5c02e933c1a7ae854f634d30b6e5063447917
Dmitrii Bordukov <dabordukov@mail.ru>
2025-12-01 19:33:30 +0300

    fix(profiler): fix test_executable doesn't run executable

--- amphimixis/profiler.py
+++ amphimixis/profiler.py
@@ -72,7 +72,9 @@ class Profiler:
     def test_executable(self) -> bool:
         """Checks if executable runs and returns no errors"""
 
-        error, stdout, stderr = self.shell.run(f"cd {self.build.build_path}")
+        error, stdout, stderr = self.shell.run(
+            f"cd {self.build.build_path}", f"./{self.executable}"
+        )
         if error != 0:
             error_message = "STDERR: " + "".join(line for cmd in stdout for line in cmd)
             error_message += "STDOUT: " + "".join(
