f9c10199c47ec478ebc858f86f3f6665b434e9c1
ebzych <bzychkin.egor@gmail.com>
2025-12-02 05:15:31 +0300

    refactor(tests): refactor tests directory hierarchy, delete __init__ files in tests

--- tests/test_toolchain_manager/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Tests of ToolchainManager"""
-
-from tests.test_toolchain_manager.test_parsing_config_file import *  # noqa: F403
-from tests.test_toolchain_manager.test_add_toolchain_and_sysroot import *  # noqa: F403
