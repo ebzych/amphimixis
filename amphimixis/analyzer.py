7bbbd0796ce203ad0d7917c9fd36e1ef4163de44
Julia <juliaadod@gmail.com>
2025-12-23 22:24:11 +0300

    analyzer(refactor): change build systems results output for configurator

--- amphimixis/analyzer.py
+++ amphimixis/analyzer.py
@@ -19,19 +19,18 @@ build_systems_list = {
     "bazel": ["**/*bazel*", "**/BUILD", "**/WORKSPACE"],
     "make": ["**/Makefile", "**/makefile"],
     "SCons": ["**/*SCons*"],
-    "autotools": ["**/configure.ac", "**/configure.in"],
-    "needs_bootstrap": ["bootstrap"],
+    "autoconf": ["**/configure.ac", "**/configure.in"],
 }
 
 
 def analyze(project: general.Project):
     """Analyzes project and collects its information"""
 
-    results = {
+    results: dict[str, list[str] | str | None] = {
         "tests": [],
         "benchmarks": [],
         "ci": [],
-        "build_systems": {k: False for k in build_systems_list},
+        "build_systems": [],
         "dependencies": [],
     }
 
@@ -103,15 +102,17 @@ def _search_ci(proj_path, results):
 
 
 def _search_build_systems(proj_path, results):
-    _logger.info("build systems:")
+    _logger.info("build_systems:")
     found = False
     for system, patterns in build_systems_list.items():
         for pat in patterns:
             matched_paths = _find_paths(proj_path, pat, dirs_only=False)
             matched_files = [p for p in matched_paths if path.isfile(p)]
             if matched_files:
-                results["build_systems"][system] = True
-                _logger.info("  %s", system)
+                if system not in results["build_systems"]:
+                    results["build_systems"].append(system)
+                    _logger.info("  %s", system)
+
                 found = True
                 break
 
@@ -131,8 +132,6 @@ def _search_dependencies(proj_path, results):
 
 
 def _third_party_dependencies(proj_path, results):
-    # third party dependencies
-
     dep_path = path.join(proj_path, "third_party")
     if path.exists(dep_path):
         dirs = [d for d in listdir(dep_path) if path.isdir(path.join(dep_path, d))]
@@ -143,21 +142,21 @@ def _third_party_dependencies(proj_path, results):
 
 
 def _cmake_dependencies(proj_path, results):
-    # cmake dependencies
+    if "cmake" not in results["build_systems"]:
+        return
 
-    if results["build_systems"]["cmake"] is True:
-        file_path = path.join(proj_path, "CMakeLists.txt")
-        if not path.isfile(file_path):
-            _logger.info("  no CMakeLists.txt in project root")
-            return
+    file_path = path.join(proj_path, "CMakeLists.txt")
+    if not path.isfile(file_path):
+        _logger.info("  no CMakeLists.txt in project root")
+        return
 
-        with open(file_path, "r", encoding="utf8") as file:
-            text = file.read()
+    with open(file_path, "r", encoding="utf8") as file:
+        text = file.read()
 
-        text = re.sub(r"#.*", "", text)
-        pattern = r"find_package\s*\(\s*([\w+-]+)"
-        packages = re.findall(pattern, text, flags=re.IGNORECASE)
-        for package in packages:
-            if package not in results["dependencies"]:
-                results["dependencies"].append(package)
-                _logger.info("  %s", package)
+    text = re.sub(r"#.*", "", text)
+    pattern = r"find_package\s*\(\s*([\w+-]+)"
+    packages = re.findall(pattern, text, flags=re.IGNORECASE)
+    for package in packages:
+        if package not in results["dependencies"]:
+            results["dependencies"].append(package)
+            _logger.info("  %s", package)
