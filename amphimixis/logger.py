d2216dfc665a52669343a701bc9335fc707f8fee
ebzych <bzychkin.egor@gmail.com>
2026-03-16 14:20:52 +0300

    feat(logger): add parameter 'dummy' to setup_logger() for getting non-logging logger

--- amphimixis/logger.py
+++ amphimixis/logger.py
@@ -4,16 +4,21 @@ import logging
 
 LOG_FILE_NAME = "amphimixis.log"
 
-
-def setup_logger(name: str) -> logging.Logger:
+def setup_logger(name: str, dummy_logging: bool = False,
+                 console_logging: bool = False) -> logging.Logger:
     """Function to create logger with specified name"""
+
+    if dummy_logging:
+        logging.basicConfig(handlers=[logging.NullHandler()])
+        return logging.getLogger(name.upper())
 
     if not logging.getLogger().hasHandlers():
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
             handlers=[
-                logging.FileHandler(LOG_FILE_NAME, encoding="utf-8"),
+                logging.FileHandler(LOG_FILE_NAME, encoding="utf-8") \
+                if not console_logging else logging.Handler(),
             ],
         )
 
