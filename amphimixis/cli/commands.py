48fa2131742efd2efd7492833c64375427312ec7
Dmitrii Bordukov <dabordukov@mail.ru>
2026-03-20 20:33:56 +0300

    fix(cli): fix incorrect profile_all cwd

--- amphimixis/cli/commands.py
+++ amphimixis/cli/commands.py
@@ -62,9 +62,7 @@ def run_profile(
 
     for build in project.builds:
         profiler_ = Profiler(project, build, ui)
-        if not profiler_.profile_all(
-            path.join("~/amphimixis", path.basename(project.path))
-        ):
+        if not profiler_.profile_all():
             ui.mark_failed()
             return False
         profiler_.save_stats()
