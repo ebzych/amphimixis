f97ec74ba7abd97d278807c5cf227046aa1f3d02
Michael Mishin <kanalpoigrammisha@gmail.com>
2026-03-21 14:47:51 +0300

    refactor(general): add build_system, runner, port constant fields

--- amphimixis/general/constants.py
+++ amphimixis/general/constants.py
@@ -1,4 +1,7 @@
 """Amphimixis constants"""
 
+DEFAULT_PORT = 22
+DEFAULT_RUNNER = "make"
+DEFAULT_BUILD_SYSTEM = "CMake"
 AMPHIMIXIS_DIRECTORY_NAME = "amphimixis"
 ANALYZED_FILE_NAME = "amphimixis.analyzed"
