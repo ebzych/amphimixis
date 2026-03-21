3655de96cdbe35527ba39a303471d27b6fa1fc03
ebzych <bzychkin.egor@gmail.com>
2026-03-21 19:26:15 +0300

    feat(Ninja): implement low-level build system Ninja

--- amphimixis/build_systems/build_systems.py
+++ amphimixis/build_systems/build_systems.py
@@ -3,15 +3,17 @@
 
 from amphimixis.build_systems.cmake import CMake
 from amphimixis.build_systems.make import Make
+from amphimixis.build_systems.ninja import Ninja
 from amphimixis.general.general import BuildSystem, ILowLevelBuildSystem
 
 # First element -- class of matched build system,
 # other are runners for this build system in priority
 build_systems_dict: dict[str, list[type[BuildSystem]]] = {
-    "cmake": [CMake, Make],
+    "cmake": [CMake, Ninja, Make],
     "make": [Make],
 }
 
 runners_dict: dict[str, type[ILowLevelBuildSystem]] = {
     "make": Make,
+    "ninja": Ninja,
 }
