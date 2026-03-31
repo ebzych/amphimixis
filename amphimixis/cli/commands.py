f0078c124aa09c30bc4e78ba8b41975e51c8dda3
Dmitrii Bordukov <dabordukov@mail.ru>
2026-03-31 16:15:39 +0300

    fix(cli): fix scriptout matching

--- amphimixis/cli/commands.py
+++ amphimixis/cli/commands.py
@@ -131,46 +131,58 @@ def show_profiling_result(project: general.Project):
 
     obj: ProjectStats = tools.load_project_stats(project)
 
-    if not obj:
+    if not obj or not any(
+        obj[build][exe].executable_run_success
+        for build in obj.keys()
+        for exe in obj[build]
+    ):
         print("\n[!] No profiling data (.scriptout files) were generated.")
         print("\tPlease check amphimixis.log for details.")
-    elif len(obj.keys()) == 1:
+        return
+
+    if len(obj.keys()) == 1:
         print("\n[i] Only one profiling result was generated.")
         print("\tTo compare two results, run profiling again with a different build.")
         print("\tOnce you have two .scriptout files, compare them with:")
         print("\tamixis --compare <file1.scriptout> <file2.scriptout>")
-    else:
-        key1, key2 = list(obj.keys())[:2]
-        executable = None
-        for exe in obj[key1]:
-            if exe in obj[key2]:
-                executable = exe
-                break
+        return
 
-        if executable is None:
-            print(
-                "\tThere is no profiling results for the same executable in different build"
-            )
-            print("\b Maybe you should check profiling errors")
-            return
+    def _find_matching_exe() -> tuple[str, str, str]:
+        found_build1 = None
+        found_build2 = None
+        found_executable = None
+        build_keys = list(obj.keys())
+        for build1_index in range(len(build_keys) - 1):
+            for build2_key in build_keys[build1_index + 1 :]:
+                build1 = obj[build_keys[build1_index]]
+                build2 = obj[build2_key]
 
-        build1_name = obj[key1][executable].build_name
-        build2_name = obj[key2][executable].build_name
+                exe_keys = set(obj[build_keys[build1_index]]) | set(obj[build2_key])
 
-        if build1_name is None or build2_name is None:
-            print("Currupted project.stats file")
-            return
+                for exe in exe_keys:
+                    if (
+                        build1[exe].executable_run_success
+                        and build2[exe].executable_run_success
+                    ):
+                        found_build1 = build_keys[build1_index]
+                        found_build2 = build2_key
+                        found_executable = exe
+                        return found_build1, found_build2, found_executable
+        return "", "", ""
 
-        file1 = (
-            tools.build_filename(build1_name, executable) + constants.PERF_SCRIPT_EXT
+    build1, build2, exe = _find_matching_exe()
+    if not all([build1, build2, exe]):
+        print(
+            "\tThere is no profiling results for the same executable in different build"
         )
+        print("\b Maybe you should check profiling errors")
+        return
 
-        file2 = (
-            tools.build_filename(build2_name, executable) + constants.PERF_SCRIPT_EXT
-        )
+    file1 = tools.build_filename(build1, exe) + constants.PERF_SCRIPT_EXT
+    file2 = tools.build_filename(build2, exe) + constants.PERF_SCRIPT_EXT
 
-        print("\n[>] To compare two profiling results, use:")
-        print(f"\tamixis --compare {file1} {file2}")
+    print("\n[>] To compare two profiling results, use:")
+    print(f"\tamixis --compare {file1} {file2}")
 
 
 def setup_profiling_environment(project: general.Project, ui: general.IUI) -> bool:
