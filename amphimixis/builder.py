a54c5d5daa51ccb530da5f99d1a02a3b673b3b29
ebzych <bzychkin.egor@gmail.com>
2026-04-03 04:33:37 +0300

    refactor(Builder): fix spelling mistake :)

--- amphimixis/builder.py
+++ amphimixis/builder.py
@@ -209,6 +209,6 @@ if __name__ == "__main__":
     )
 
     if Builder.build_for_linux(project_, build_):
-        print("Successfully builded!")
+        print("Successfully built!")
     else:
         print("Build failed!")
