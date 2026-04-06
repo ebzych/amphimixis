2fd40709eba7f7669b3e6e194683508a17bed4a3
ebzych <bzychkin.egor@gmail.com>
2026-04-07 02:29:04 +0300

    feat(opencode): analyzing tool calling by amphimixis CLI

--- opencode/tools/amphimixis.analyze.ts
+++ opencode/tools/amphimixis.analyze.ts
@@ -1,17 +1,20 @@
-import { tool } from "@opencode-ai/plugin"
-import path from "path"
-import process from "process"
+import { tool } from '@opencode-ai/plugin';
+import process from 'process';
+import path from 'path';
 
 export default tool({
-    description: "Analyze project repository: find CI, tests, benchmarks, dependencies, documentation, build systems",
+    description: 'Analyze project repository: find CI, tests, benchmarks, dependencies, documentation, build systems',
     args: {
-        project_path: tool.schema.string().describe("Path to repository of analyzing project"),
+        project_path: tool.schema.string().describe('Path to repository of analyzing project'),
+        config: tool.schema.string().optional().describe('Path to config file. If not specified, uses input.yml in project directory if exists'),
     },
     async execute(args) {
-        const config_dir = process.env.XDG_CONFIG_HOME != undefined ? process.env.XDG_CONFIG_HOME : path.join(process.env.HOME as string, ".config")
-        const opencode_tools_dir = path.join(config_dir, "opencode/tools")
-        const script = path.join(opencode_tools_dir, "amphimixis/analyzer.py")
-        const result = await Bun.$`PYTHONPATH=${opencode_tools_dir} python3 ${script} ${args.project_path}`.text()
-        return result.trim()
+        const config_dir = process.env.XDG_CONFIG_HOME != undefined ? process.env.XDG_CONFIG_HOME : path.join(process.env.HOME as string, '.config');
+        const opencode_tools_dir = path.join(config_dir, 'opencode/tools');
+        const activate_path = path.join(opencode_tools_dir, '.venv/bin/activate');
+        const configArg = args.config ? `--config=${args.config}` : '';
+
+        const result = await Bun.$`bash -c "source ${activate_path} && PYTHONPATH=${opencode_tools_dir} amixis --analyze ${args.project_path} ${configArg}"`.text();
+        return result.trim();
     },
-})
+});
