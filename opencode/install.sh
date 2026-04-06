0062591712d3989c44829e04aa072f3a59b4d033
ebzych <bzychkin.egor@gmail.com>
2026-04-07 02:29:04 +0300

    feat(opencode): analyzing tool calling by amphimixis CLI

--- opencode/install.sh
+++ opencode/install.sh
@@ -13,4 +13,9 @@ cp -a $SELF_PATH/amphimixis.md $CONFIG_HOME/opencode/agents
 
 cp -a $SELF_PATH/tools/amphimixis.analyze.ts $SELF_PATH/tools/amphimixis.build.ts $SELF_PATH/tools/amphimixis.profile.ts $CONFIG_HOME/opencode/tools
 
-ln -sdf $SELF_PATH/../amphimixis $CONFIG_HOME/opencode/tools/amphimixis
+ln -sdf $SELF_PATH/.. $CONFIG_HOME/opencode/tools/amphimixis
+
+cd $CONFIG_HOME/opencode/tools
+python3 -m venv .venv
+source .venv/bin/activate
+pip install ./amphimixis
