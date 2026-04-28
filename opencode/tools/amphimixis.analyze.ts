8bdcd7457bd70fb4aa821fc3fd7065c6874d999a
ebzych <bzychkin.egor@gmail.com>
2026-04-29 02:43:39 +0300

    refactor(tools/analyze): format

--- opencode/tools/amphimixis.analyze.ts
+++ opencode/tools/amphimixis.analyze.ts
@@ -3,17 +3,23 @@ import process from "process";
 import path from "path";
 
 export default tool({
-    description: 'Analyze project repository: find CI, tests, benchmarks, dependencies, documentation, build systems',
-    args: {
-        project_path: tool.schema.string().describe('Path to repository of analyzing project'),
-    },
-    async execute(args) {
-        const config_dir = process.env.XDG_CONFIG_HOME != undefined ? process.env.XDG_CONFIG_HOME : path.join(process.env.HOME as string, '.config');
-        const opencode_tools_dir = path.join(config_dir, 'opencode', 'tools');
-        const amixis = path.join(opencode_tools_dir, '.venv', 'bin', 'amixis');
-        let cmd = [amixis, 'analyze', args.project_path];
+  description:
+    "Analyze project repository: find CI, tests, benchmarks, dependencies, documentation, build systems",
+  args: {
+    project_path: tool.schema
+      .string()
+      .describe("Path to repository of analyzing project"),
+  },
+  async execute(args) {
+    const config_dir =
+      process.env.XDG_CONFIG_HOME != undefined
+        ? process.env.XDG_CONFIG_HOME
+        : path.join(process.env.HOME as string, ".config");
+    const opencode_tools_dir = path.join(config_dir, "opencode", "tools");
+    const amixis = path.join(opencode_tools_dir, ".venv", "bin", "amixis");
+    let cmd = [amixis, "analyze", args.project_path];
 
-        const result = await Bun.$`${cmd}`.text();
-        return result.trim();
-    },
+    const result = await Bun.$`${cmd}`.text();
+    return result.trim();
+  },
 });
