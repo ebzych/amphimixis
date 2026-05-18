151bfb046455f1a8450c07a16bdb1906d583bf53
ebzych <bzychkin.egor@gmail.com>
2026-05-18 21:59:58 +0300

    ci(black.sh): add --diff for more information

--- ci/black.sh
+++ ci/black.sh
@@ -4,6 +4,6 @@ NC='\e[0m'
 
 echo -e "${BLUE}Running Black code formatter...${NC}"
 mapfile -t files < <(git ls-files ':/*.py')
-uv run black --check "${files[@]}"
+uv run black --check --diff "${files[@]}"
 
 
