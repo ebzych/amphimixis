1b49f719c3bbe86b48a05d6bece56b9c473bc015
ebzych <bzychkin.egor@gmail.com>
2026-06-12 06:39:15 +0300

    ci(mdl.sh): adapt to agent specs in markdown: no linting

--- ci/mdl.sh
+++ ci/mdl.sh
@@ -3,6 +3,5 @@ BLUE='\e[34m'
 NC='\e[0m'
 
 echo -e "${BLUE}Running Markdown linter...${NC}"
-mapfile -t files < <(git ls-files ':/*.md')
+mapfile -t files < <(git ls-files ':/*.md' | grep -v 'opencode')
 mdl -r '~MD029','~MD013','~MD033' "${files[@]}"
-
