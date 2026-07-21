69ae501150600e80fe239a03e5c6f39fca2559dc
ebzych <bzychkin.egor@gmail.com>
2026-07-21 14:11:17 +0300

    refactor(amixis.__main__): simplify code

--- amphimixis/amixis/__main__.py
+++ amphimixis/amixis/__main__.py
@@ -66,9 +66,7 @@ def _main() -> bool:
         return False
 
     project = None
-    if args.command in ("run", "analyze", "build", "profile") and not hasattr(
-        args, "vector"
-    ):
+    if hasattr(args, "path"):
         if not args.path:
             parser.print_help()
             return False
